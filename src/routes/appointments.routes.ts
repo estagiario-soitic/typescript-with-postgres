5dafd8f5d83fc77ca6144122a41ec018ace4db8e
Caio Capua <caiocapua@hotmail.com>
2020-04-29 19:26:04 -0300

    Projeto typescript

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentsServices from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (request, response) => {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const appointments = await appointmentsRepository.find();
+
+    return response.json(appointments);
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+    // provider qual o nome do prestador de servicos que ira atender ele.
+    try {
+        const { provider_id, date } = request.body;
+
+        const parsedDate = parseISO(date);
+
+        const createAppointments = new CreateAppointmentsServices();
+
+        const appointment = await createAppointments.execute({
+            date: parsedDate,
+            provider_id,
+        });
+
+        return response.json(appointment);
+    } catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
+});
+
+export default appointmentsRouter;
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import { hash } from 'bcryptjs';
+import CreateUserServices from '../services/CreateUserService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+    try {
+        const { name, email, password } = request.body;
+
+        const createUser = new CreateUserServices();
+
+        const hashedPassword = await hash(password, 8);
+
+        const user = await createUser.execute({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        delete user.password;
+
+        return response.json(user);
+    } catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
+});
+
+export default usersRouter;
