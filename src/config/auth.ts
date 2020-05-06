97b6ad8d17e814ab5536ee2f3af610df0b815be3
Caio Capua <caiocapua@hotmail.com>
2020-05-06 11:39:50 -0300

    Upload de arquivos com multer

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
     jwt: {
         secret: 'bc8467d1fb948ae95538589f05818e15',
-        expiresIn: 'id',
+        expiresIn: '1d',
     },
 };
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import UploadConfig from '../config/upload';
 import CreateUserServices from '../services/CreateUserService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+const upload = multer(UploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -22,5 +27,16 @@ usersRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: err.message });
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        console.log(request.file);
+
+        return response.json({ ok: true });
+    },
+);
 
 export default usersRouter;
